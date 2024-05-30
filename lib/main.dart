6f696d4798b0dd31a520e007b616030fb95876a1
Danilo Bitencourt <danilo300600@gmail.com>
2024-05-30 15:46:25 -0300

    Organização Inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
+
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'Vamos Cozinhar?',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
- 
+
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
- 
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: const Text('Vamos Cozinhar?'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Navegar é preciso!!'),
       ),
     );
