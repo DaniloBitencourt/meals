4903079eb3fb5ed4acd7e15dea15991ba56fa823
Danilo Bitencourt <danilo300600@gmail.com>
2024-05-31 18:25:45 -0300

    Definição do card de cada item

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,9 +1,10 @@
-enum Complexity{
+enum Complexity {
   simple,
   medium,
   difficult,
 }
-enum Cost{
+
+enum Cost {
   cheap,
   fair,
   expensive,
@@ -24,8 +25,7 @@ class Meal {
   final Complexity complexity;
   final Cost cost;
 
-  const Meal(
-    {
+  const Meal({
     required this.id,
     required this.categories,
     required this.title,
@@ -36,8 +36,34 @@ class Meal {
     required this.isGlutenFree,
     required this.isLactoseFree,
     required this.isVegan,
-    required this.isVegetarian, 
+    required this.isVegetarian,
     required this.complexity,
     required this.cost,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.simple:
+        return 'Simples';
+      case Complexity.medium:
+        return 'Normal';
+      case Complexity.difficult:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.cheap:
+        return 'Barato';
+      case Cost.fair:
+        return 'Justo';
+      case Cost.expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecido';
+    }
+  }
 }
