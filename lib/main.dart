f80ef37f6702097eb234adce9d620debade914f5
Danilo Bitencourt <danilo300600@gmail.com>
2024-05-31 21:04:33 -0300

    Adição do Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/settings_screen.dart';
 import 'utils/app_routes.dart';
 
 void main() {
@@ -72,6 +73,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (ctx) => const TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => const SettingsScreen(),
       },
     );
   }
