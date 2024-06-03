3af8156590f9047eea2b98d2805b4b1ee07047ba
Danilo Bitencourt <danilo300600@gmail.com>
2024-06-03 12:03:32 -0300

    Definindo a tela de Favoritos e a opção de favoritar uma refeição

--- lib/main.dart
+++ lib/main.dart
@@ -24,22 +24,37 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   Settings settings = Settings();
   List<Meal> _availableMeals = dummyMeals;
+  List<Meal> _favoriteMeals = [];
 
-  void _filterMeals (Settings settings){
+  void _filterMeals(Settings settings) {
     setState(() {
       this.settings = settings;
-      _availableMeals = dummyMeals.where((meal){
+      _availableMeals = dummyMeals.where((meal) {
         final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
         final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
         final filterVegan = settings.isVegan && !meal.isVegan;
         final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
-        return !filterGluten && !filterLactose && !filterVegan && !filterVegetarian; 
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
       }).toList();
     });
   }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+  bool _isFavorite(Meal meal){
+    return _favoriteMeals.contains(meal);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -78,9 +93,10 @@ class _MyAppState extends State<MyApp> {
             color: Colors.black,
           ),
         ),
-        scaffoldBackgroundColor: const Color.fromRGBO(255, 254, 229, 1), 
+        scaffoldBackgroundColor: const Color.fromRGBO(255, 254, 229, 1),
         cardColor: tema.scaffoldBackgroundColor,
-        drawerTheme: DrawerThemeData(backgroundColor: tema.scaffoldBackgroundColor),
+        drawerTheme:
+            DrawerThemeData(backgroundColor: tema.scaffoldBackgroundColor),
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.pink,
           titleTextStyle: TextStyle(
@@ -96,13 +112,13 @@ class _MyAppState extends State<MyApp> {
             style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all(Colors.purple),
         )),
-        //TextButton é o botão sem formatação
       ),
       routes: {
-        AppRoutes.HOME: (ctx) => const TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(_availableMeals),
-        AppRoutes.MEAL_DETAIL: (ctx) => const MealDetailScreen(),
-        AppRoutes.SETTINGS: (ctx) => SettingsScreen(_filterMeals,settings),
+        AppRoutes.HOME: (ctx) => TabsScreen(_favoriteMeals),
+        AppRoutes.CATEGORIES_MEALS: (ctx) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(_toggleFavorite, _isFavorite),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(_filterMeals, settings),
       },
     );
   }
